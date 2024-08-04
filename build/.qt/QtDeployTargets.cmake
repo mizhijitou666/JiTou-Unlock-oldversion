b990eef8675d9793d320cee3d347be77c829fcf4
mizhijitou666 <a1249863413@gmail.com>
2024-08-04 19:06:57 +0800

    del unuse file

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_JiTou-Unlock_FILE C:/Users/mizhijitou/Desktop/JiTou-Unlock-master/build/JiTou-Unlock.exe)
-set(__QT_DEPLOY_TARGET_JiTou-Unlock_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
--- build/CMakeFiles/JiTou-Unlock.dir/JiTou-Unlock_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/JiTou-Unlock.dir/JiTou-Unlock_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- C:\Users\mizhijitou\Desktop\JiTou-Unlock-master\build\JiTou-Unlock_autogen\EWIEGA46WW\qrc_resources.cpp
